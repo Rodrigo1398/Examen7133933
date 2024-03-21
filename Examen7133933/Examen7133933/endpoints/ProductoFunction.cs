946ed288c27acf0822ea078fa71959355ae77928
RODRIGO <j.rodrigo.a.colque@gmail.com>
2024-03-20 20:49:54 -0400

    personal

--- Examen7133933/Examen7133933/endpoints/ProductoFunction.cs
+++ Examen7133933/Examen7133933/endpoints/ProductoFunction.cs
@@ -17,7 +17,6 @@ namespace Examen7133933.endpoints
         [Function("ProductoFunction")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
